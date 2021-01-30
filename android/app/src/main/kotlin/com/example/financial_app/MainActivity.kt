4d076774b79e1830a33152eac0bfb8e131e1807d
mosesmbadi <57032428+mosesmbadi@users.noreply.github.com>
2021-01-30 14:01:00 +0300

    landing page

---
+++ android/app/src/main/kotlin/com/example/financial_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wallety
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:wallety/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'Wallety',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));

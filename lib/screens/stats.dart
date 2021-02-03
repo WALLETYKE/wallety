fd8ea935356c55ce833f43fe5254596f6bbf4c22
mosesmbadi <57032428+mosesmbadi@users.noreply.github.com>
2021-02-03 14:28:51 +0300

    first commit of teh statspage

--- lib/screens/stats.dart
+++ lib/screens/stats.dart
@@ -9,6 +9,55 @@ class Header extends StatelessWidget {
     return Scaffold(
       body: Container(
         color: Colors.red,
+        child: Column(
+          children:[
+            Container(
+              height: 150,
+              width: double.infinity,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: 150,
+              width: double.infinity,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Center(
+                    //height: 20,
+                   // width: 30,
+                    child: Text('Jan'),
+                  ),
+                  
+                  Row(
+                      children: [
+                         Container(
+                           height: 30,
+                           width: 30,
+                           child: Text('earned'),
+                         ),
+                         Text('spent'),
+                        Text('saved'),
+                        ],
+                    ),
+                  
+
+                  Row(
+                    children: [
+                      Text('2000'),
+                      Text('3500'),
+                      Text('1500'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 150,
+              width: double.infinity,
+              color: Colors.blue,
+            ),
+          ],
+        ),
       ),
     );
   }

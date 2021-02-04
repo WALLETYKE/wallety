7727e71b3da8afeedc1b2b596a736de338631a90
mosesmbadi <57032428+mosesmbadi@users.noreply.github.com>
2021-02-04 14:29:10 +0300

    Fixed navigation

--- old/lib/screens/HomePage.dart
+++ old/lib/screens/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wallety/screens/pushed_screen.dart';
 
 class HomePage extends StatelessWidget {
   static const route = '/first';
@@ -6,8 +7,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Expense')),
-      body: Stack(children: [
+        body: Stack(children: [
       Column(
         children: [
           Container(

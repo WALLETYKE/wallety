34aed4c9f775e16ea09bd46a839ac4ca8baa1b6f
mosesmbadi <57032428+mosesmbadi@users.noreply.github.com>
2021-02-10 12:16:17 +0300

    updated the profiles page

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,5 +1,7 @@
 //import 'package:auto_route/auto_route.dart';
 //import 'package:wallety/router/router.dart';
+
+
 import 'package:flutter/material.dart';
 
 
@@ -418,30 +420,12 @@ class Drawer extends StatelessWidget {
   @override
   Widget build(BuildContext contx) {
     return Container(
+      
       color: Colors.amber[600],
       child: Column(
         children: <Widget>[
-          AppBar(automaticallyImplyLeading: false, title: Text('menu')),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
-            onTap: () {
-              Navigator.pushReplacementNamed(contx, '/home');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('My Profile'),
-            onTap: () {
-              Navigator.pushReplacementNamed(contx, '/profile');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.school),
-            title: Text('My Education'),
-            onTap: () {
-              Navigator.pushReplacementNamed(contx, '/education');
-            },
+          DrawerHeader(
+            child: Center(),
           ),
         ],
       ),
--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -1,12 +1,230 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
 
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
+      body: SafeArea(
+        child: Column(
+          
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    "https://images.pexels.com/photos/3321540/pexels-photo-3321540.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                  ),
+                  fit: BoxFit.cover
+                )
+              ),
+            child: Container(
+              width: double.infinity,
+              height: 200,
+              child: Container(
+                alignment: Alignment(0.0,2.5),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.pexels.com/photos/3280901/pexels-photo-3280901.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                  ),
+                  radius: 60.0,
+                ),
+              ),
+            ),
+            ),
+
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              "Eunice Kahiga"
+              ,style: TextStyle(
+                fontSize: 25.0,
+                color:Colors.blueGrey,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w400
+            ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Eldoret, Kenya"
+              ,style: TextStyle(
+                fontSize: 18.0,
+                color:Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300
+            ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Saloonist and a babysitter"
+              ,style: TextStyle(
+                fontSize: 15.0,
+                color:Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300
+            ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 8.0),
+
+              elevation: 2.0,
+              child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12,horizontal: 30),
+                  child: Text("Stats",style: TextStyle(
+                      letterSpacing: 2.0,
+                      fontWeight: FontWeight.w300
+                  ),))
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Saloonist || Baby Sitter"
+              ,style: TextStyle(
+                fontSize: 18.0,
+                color:Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300
+            ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 8.0),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text("Goal",
+                            style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600
+                            ),),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text("Ksh 20,000",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w300
+                            ),)
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child:
+                      Column(
+                        children: [
+                          Text("Saved",
+                            style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600
+                            ),),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text("ksh 10,000",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w300
+                            ),)
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(
+                  onPressed: (){
+
+                  },
+                  shape:  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0),
+                  ),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [Colors.pink,Colors.redAccent]
+                      ),
+                      borderRadius: BorderRadius.circular(30.0),
+
+                    ),
+                    child: Container(
+                      constraints: BoxConstraints(maxWidth: 100.0,maxHeight: 40.0,),
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Contact me",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12.0,
+                            letterSpacing: 2.0,
+                            fontWeight: FontWeight.w300
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: (){
+
+                  },
+                  shape:  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0),
+                  ),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [Colors.pink,Colors.redAccent]
+                      ),
+                      borderRadius: BorderRadius.circular(80.0),
+
+                    ),
+                    child: Container(
+                      constraints: BoxConstraints(maxWidth: 100.0,maxHeight: 40.0,),
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Portfolio",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12.0,
+                            letterSpacing: 2.0,
+                            fontWeight: FontWeight.w300
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
